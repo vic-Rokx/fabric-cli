733b136d0e98f0354e15b126c98e43356e9b0447
Vic-August Rokx-Nellemann <93738822+vic-Rokx@users.noreply.github.com>
2025-06-20 19:50:17 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+set -e
+
 # macOS install script
 INSTALL_DIR="/usr/local/bin"
+REPO="vic-Rokx/fabric-cli"
+VERSION="v1.0.0"
+TARBALL="fabric.tar.gz"
+DIR="fabric-cli-1.0.0"
 
-# Create /usr/local/bin if it doesn't exist
-sudo mkdir -p "$INSTALL_DIR"
-
-# Copy binary
-sudo cp myprogram "$INSTALL_DIR/"
-sudo chmod +x "$INSTALL_DIR/fabric"
-
-echo "fabric installed to $INSTALL_DIR"
-echo "Make sure $INSTALL_DIR is in your PATH"
+echo "Installing myprogram..."
+curl -L -o /tmp/$TARBALL https://github.com/vic-Rokx/fabric-cli/archive/refs/tags/v1.0.0.tar.gz
+cd /tmp
+tar -xzf $TARBALL
+cd $DIR
+sudo cp fabric $INSTALL_DIR/
+sudo chmod +x $INSTALL_DIR/fabric
+rm -rf /tmp/$DIR /tmp/$TARBALL
+echo "myprogram installed successfully!"
